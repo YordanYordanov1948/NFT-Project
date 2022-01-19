94cefb818c911bee53b42afd68cd219966ee47f7
Yordan <yordan_yordanov001@abv.bg>
2022-01-19 21:03:24 +0200

    logo component added

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../../assets/images/logo-muted.svg";
+
+function Logo() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <img src={logo} alt="logo"></img>
+    </div>
+  );
+}
+
+export default Logo;
