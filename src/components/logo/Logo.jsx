0b4e700f37ca68451174d7bf503fa4a9dff7222f
Yordan <yordan_yordanov001@abv.bg>
2022-01-21 12:55:36 +0200

    fix

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../../../public/images/logo.svg";
-import logomuted from "../../../public/images/logo-muted.svg";
 
 function Logo({ type }) {
-  if (type) {
-    return <img src={logomuted} />;
-  }
-  return <img src={logo} alt="logo" />;
+  return (
+    <div>
+      <img src="images/logo.svg" />
+      {type && <img src="image/logo-muted.svg" />}
+    </div>
+  );
 }
 
 export default Logo;
