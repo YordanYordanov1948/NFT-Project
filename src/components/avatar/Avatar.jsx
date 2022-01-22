ec25287cff03d4d8a845a81ce2836feafd0d0eb2
Yordan <yordan_yordanov001@abv.bg>
2022-01-22 21:24:58 +0200

    fix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import styles from "./Avatar.module.scss";
+import classNames from "classnames";
 
 function Avatar({ url, verified, size }) {
   return (
-    <div
-      className="avatar"
-      style={{ width: 90 || { size }, height: 90 || { size } }}
-    >
-      {" "}
-      <img className="image" src="images/avatar.png" />
-      {verified && verified == false}
+    <div className="avatar" style={size}>
+      <img className={classNames(styles.image)} src="images/avatar.png" />
       <img className="badge" src="images/verified.svg" />
     </div>
   );
