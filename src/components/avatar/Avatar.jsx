2564dd8bb55ff4f221708a05156a0f02c25405f5
Yordan <yordan_yordanov001@abv.bg>
2022-01-22 19:44:48 +0200

    style size prop

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 
 function Avatar({ url, verified, size }) {
-  const mystyle = {
-    width: { size },
-    height: { size },
-  };
-
   return (
-    <div className="avatar" style={mystyle}>
+    <div className="avatar" style={{ size }}>
       {<img className="image" src="images/avatar.png" />}
       <img className="badge" src="images/verified.svg" />
     </div>
