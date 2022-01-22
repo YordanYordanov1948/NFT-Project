d7d58e6977ff3d7fdd11fb09b0281f4351e00d03
Yordan <yordan_yordanov001@abv.bg>
2022-01-23 00:20:26 +0200

    fix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -4,7 +4,7 @@ export default function Avatar({ url, verified = false, size = 90 }) {
   return (
     <div>
       {verified}
-      <div className="avatar" style={size}>
+      <div className="avatar" style={{ size }}>
         <img className="image" src="images/avatar.png" />
         <img className="badge " src="images/verified.svg" />
       </div>
