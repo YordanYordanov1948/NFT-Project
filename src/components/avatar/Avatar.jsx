7d833eb3293213c6d0fe68bb6533f959c3ce86cd
Yordan <yordan_yordanov001@abv.bg>
2022-01-22 20:46:21 +0200

    size prop fix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Avatar({ url, verified, size }) {
   return (
-    <div className="avatar" style={{ width: 90 || size, height: 90 || size }}>
+    <div
+      className="avatar"
+      style={{ width: 90 || { size }, height: 90 || { size } }}
+    >
       {" "}
       <img className="image" src="images/avatar.png" />
       {verified || verified === false}
