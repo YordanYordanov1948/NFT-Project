ef96eebe9cb5c16030d464de98cd875d1b2dd3cf
Yordan <yordan_yordanov001@abv.bg>
2022-01-23 00:26:50 +0200

    fix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Avatar({ url, verified = false, size = 90 }) {
+export default function Avatar({ url, verified = false, size = "90px" }) {
   return (
     <div>
       {verified}
-      <div className="avatar" style={{ size }}>
+      <div className="avatar" style={{ height: { size }, width: { size } }}>
         <img className="image" src="images/avatar.png" />
         <img className="badge " src="images/verified.svg" />
       </div>
