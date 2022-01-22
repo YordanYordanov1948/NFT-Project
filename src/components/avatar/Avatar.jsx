134ba94852097f0cb878b77344f578bad1735bbb
Yordan <yordan_yordanov001@abv.bg>
2022-01-22 22:18:24 +0200

    avatar size

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 function Avatar({ size, url, verified }) {
+  const styled = {
+    height: 90,
+    weight: 90,
+  };
   return (
-    <div className="avatar">
-      <img className="image" src={(url = "images/avatar.png")}></img>
+    <div className="avatar" style={styled || { size }}>
+      <img
+        className="image"
+        src="images/avatar.png"
+        style={styled || { size }}
+      />
+      <img className="badge" src="images/verified.svg" />
     </div>
   );
 }
