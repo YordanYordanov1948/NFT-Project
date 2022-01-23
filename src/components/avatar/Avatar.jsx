26dac40a52808ae48a0db0cbdec5e8196ca95703
Yordan <yordan_yordanov001@abv.bg>
2022-01-23 11:03:40 +0200

    img style

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -8,7 +8,11 @@ export default function Avatar({ url, verified = false, size = 90 }) {
         className="avatar"
         style={{ width: size + "px", height: size + "px" }}
       >
-        <img className="image" src="images/avatar.png" />
+        <img
+          className="image"
+          src="images/avatar.png"
+          style={{ width: 100, height: 100 }}
+        />
         <img className="badge " src="images/verified.svg" />
       </div>
     </div>
