d2a6dd78561c99aa4256a5afdcdb17cda7967e84
Yordan <yordan_yordanov001@abv.bg>
2022-01-23 20:54:12 +0200

    border radius added

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import Avatar from "../avatar/Avatar";
-import CardMui from "@mui/material/Card";
+import Card from "@mui/material/Card";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
+import millify from "millify";
+import classNames from "classnames";
+import styles from "./Card.module.scss";
 
-export default function Card({ name, likes, mediaUrl, price, currency }) {
+export default function CardComponent({
+  name,
+  likes = 0,
+  mediaUrl,
+  price,
+  currency,
+}) {
   return (
-    <CardMui className="card">
-      <Avatar />
+    <Card className={classNames(styles.card)}>
       {currency}
       <div className="media">
         {mediaUrl}
+        <Avatar />
         <img src="./images/nft.jpg/"></img>
+        <div className="likes">
+          <IconButton aria-label="add to favorites">
+            {millify(likes)}
+            <FavoriteIcon />
+          </IconButton>
+        </div>
       </div>
-      <div className="title">{name}</div>
-      <div className="price">{price}</div>
-      <div className="likes">{likes}</div>
-    </CardMui>
+      <div className={classNames(styles.title)}>{name}</div>
+      <div className={classNames(styles.price)}>
+        {" "}
+        ~ {price} {currency}
+      </div>
+    </Card>
   );
 }
