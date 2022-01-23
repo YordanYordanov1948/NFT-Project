3ff14680c0b0ae81941162160d067e5fd5a5d6d6
Yordan <yordan_yordanov001@abv.bg>
2022-01-23 20:01:59 +0200

    props added

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Avatar from "../avatar/Avatar";
+import CardMui from "@mui/material/Card";
 
-export default function Card() {
-  <div className="card "></div>;
+export default function Card({ name, likes, mediaUrl, price, currency }) {
+  return (
+    <CardMui className="card">
+      <Avatar />
+      {currency}
+      <div className="media">
+        {mediaUrl}
+        <img src="./images/nft.jpg/"></img>
+      </div>
+      <div className="title">{name}</div>
+      <div className="price">{price}</div>
+      <div className="likes">{likes}</div>
+    </CardMui>
+  );
 }
