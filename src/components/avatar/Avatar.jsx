6a01b0e52ccf7a4885b70ed1ffdafa2a72fe6361
Yordan <yordan_yordanov001@abv.bg>
2022-01-23 10:15:49 +0200

    style prop

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
 export default function Avatar({ url, verified = false, size = 90 }) {
-  const style = {
-    height: { size },
-    width: { size },
-  };
+  const style = { size };
   return (
     <div>
       {verified}
-      <div className="avatar" style={{ style }}>
+      <div className="avatar" style={style}>
         <img className="image" src="images/avatar.png" />
         <img className="badge " src="images/verified.svg" />
       </div>
