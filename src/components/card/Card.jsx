6e734fcb304e50697454d7e5f2392743dacfeb0c
Yordan <yordan_yordanov001@abv.bg>
2022-01-24 14:53:58 +0200

    react coundown component and props added

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,8 +6,11 @@ import IconButton from "@mui/material/IconButton";
 import millify from "millify";
 import classNames from "classnames";
 import styles from "./Card.module.scss";
+import Countdown from "react-countdown";
 
 export default function CardComponent({
+  name,
+  timeLeft,
   title,
   likes = 0,
   mediaUrl,
@@ -15,24 +18,39 @@ export default function CardComponent({
   currency,
 }) {
   return (
-    <Card className={classNames(styles.card)}>
-      {currency}
-      <div className="media">
-        {mediaUrl}
-        <Avatar />
-        <img src="./images/nft.jpg/" className={classNames(styles.media)}></img>
-        <div className="likes">
-          <IconButton aria-label="add to favorites">
-            {millify(likes)}
-            <FavoriteIcon />
-          </IconButton>
+    <div>
+      <Card className={classNames(styles.card)}>
+        {currency}
+        <div className="media">
+          {mediaUrl}
+          <Avatar />
+          <img
+            src="./images/nft.jpg/"
+            className={classNames(styles.media)}
+          ></img>
+          {timeLeft && (
+            <Countdown>
+              <div className="badge">
+                <img
+                  src="./images/nft.jpg/"
+                  className={classNames(styles.media)}
+                ></img>{" "}
+              </div>
+            </Countdown>
+          )}
+          <div className="likes">
+            <IconButton aria-label="add to favorites">
+              {millify(likes)}
+              <FavoriteIcon />
+            </IconButton>
+          </div>
         </div>
-      </div>
-      <div className={classNames(styles.title)}>{title}</div>{" "}
-      <div className={classNames(styles.price)}>
-        {" "}
-        ~ {price} {currency}
-      </div>
-    </Card>
+        <div className={classNames(styles.title)}>{title}</div>{" "}
+        <div className={classNames(styles.price)}>
+          {" "}
+          ~ {price} {currency}
+        </div>
+      </Card>
+    </div>
   );
 }
