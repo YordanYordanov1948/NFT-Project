fd2d7b036db23627f0f016402266714dc918b387
Yordan <yordan_yordanov001@abv.bg>
2022-01-24 15:46:16 +0200

    auctions component added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
 import Header from "../src/components/header/Header";
-import Trending from "../src/components/trending/Trending";
+import Auctions from "../src/components/auctions/Auctions";
 export default function Index() {
   return (
     <div>
       <Header />
-      <Trending />
+      <Auctions />
     </div>
   );
 }
