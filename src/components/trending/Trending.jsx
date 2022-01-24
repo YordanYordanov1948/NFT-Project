62e91dfe28a3b6e2e08ba120eebda6a1ab0c4af7
Yordan <yordan_yordanov001@abv.bg>
2022-01-24 13:55:24 +0200

    container fix

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -11,36 +11,38 @@ import styles from "./Trending.module.scss";
 
 export default function Trending({ cards = "default" }) {
   return (
-    <Container fixed>
-      <Box>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="baseline"
-        >
-          <Typography
-            variant="h1"
-            component="div"
-            gutterBottom
-            className={classNames(styles.typography)}
+    <div className={classNames(styles.wrapper)}>
+      <Container className={classNames(styles.container)} maxWidth="xl">
+        <Box>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="baseline"
           >
-            Trending
-          </Typography>
-          <Select
-            sx={{ m: 1, width: 300 }}
-            className={classNames(styles.select)}
-          >
-            <MenuItem>{cards}</MenuItem>
-          </Select>
-        </Grid>
-        <Grid container spacing={4}>
-          <CardComponent title={"Clock"} />
-          <CardComponent title={"DOGE"} />
-          <CardComponent title={"BTC"} />
-          <CardComponent title={"Litecoin"} />
-        </Grid>
-      </Box>
-    </Container>
+            <Typography
+              variant="h1"
+              component="div"
+              gutterBottom
+              className={classNames(styles.typography)}
+            >
+              Trending
+            </Typography>
+            <Select
+              sx={{ m: 1, width: 300 }}
+              className={classNames(styles.select)}
+            >
+              <MenuItem>{cards}</MenuItem>
+            </Select>
+          </Grid>
+          <Grid container spacing={4}>
+            <CardComponent title={"Clock"} />
+            <CardComponent title={"DOGE"} />
+            <CardComponent title={"BTC"} />
+            <CardComponent title={"Litecoin"} />
+          </Grid>
+        </Box>
+      </Container>
+    </div>
   );
 }
