f081c3c186bca1cef26483d22185609538c0614b
Yordan <yordan_yordanov001@abv.bg>
2022-01-25 19:13:39 +0200

    how component updated

--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -1,46 +1,41 @@
 import React from "react";
-import Step from "./Step";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import styles from "./How.module.scss";
 
-export default function How({ description, title, items = [], link }) {
+export default function How({ title, description, items = [], link }) {
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: "center",
     color: theme.palette.text.secondary,
   }));
+
   return (
     <div>
-      {" "}
       <Container>
         <Grid
           container
           direction="column"
           justifyContent="flex-start"
-          alignItems="baseline"
+          alignItems="flex-start"
         >
-          <Grid xs={2}>
+          <Grid item xs={2}>
             <Item>{title}</Item>
           </Grid>
-          <Grid xs={2}>
-            <Item>
-              {" "}
-              <Step value={items} />
-            </Item>
-          </Grid>
-          <Grid xs={6}>
+          <Grid item xs={2}>
             <Item>{description}</Item>
           </Grid>
-          <Grid xs={6}>
-            <Button variant="contained" disableElevation href={link}>
-              LEARN MORE
-            </Button>{" "}
+          <Grid item xs={2}>
+            {items}
           </Grid>
         </Grid>
+        <Button variant="contained" href={link}>
+          Link
+        </Button>
       </Container>
     </div>
   );
