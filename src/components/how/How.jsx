29d9b75571d3b13c5e999bcf6d7027d58c22794a
Yordan <yordan_yordanov001@abv.bg>
2022-01-25 23:04:37 +0200

    fix

--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -3,8 +3,7 @@ import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import styles from "./How.module.scss";
-import Step from "../step/Step";
+import Step from "./Step";
 
 export default function How({ description, title, items = [], link }) {
   const Item = styled(Paper)(({ theme }) => ({
@@ -15,32 +14,25 @@ export default function How({ description, title, items = [], link }) {
   }));
 
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="space-between"
-      alignItems="flex-start"
-    >
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>{description}</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>{title}</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-            {items}
-            <Step />
-          </Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Button variant="contained" href={link}>
-          Link
-        </Button>
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <Item>{description}</Item>
       </Grid>
+      <Grid item xs={4}>
+        <Item>{title}</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>
+          {items}
+          <Step />
+        </Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+      <Button variant="contained" href={link}>
+        Link
+      </Button>
     </Grid>
   );
 }
