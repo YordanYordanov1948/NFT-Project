cb8a02361c576f5a122cf57868241e2c1d057bdd
Yordan <yordan_yordanov001@abv.bg>
2022-01-25 22:56:03 +0200

    styling

--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import styles from "./How.module.scss";
+import Step from "../step/Step";
 
 export default function How({ description, title, items = [], link }) {
   const Item = styled(Paper)(({ theme }) => ({
@@ -14,22 +15,32 @@ export default function How({ description, title, items = [], link }) {
   }));
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <Item>xs=8</Item>
+    <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="flex-start"
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>{description}</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>{title}</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            {items}
+            <Step />
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Button variant="contained" href={link}>
+          Link
+        </Button>
       </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={8}>
-        <Item>xs=8</Item>
-      </Grid>
-      <Button variant="contained" href={link}>
-        Link
-      </Button>
     </Grid>
   );
 }
--- src/components/step/Step.jsx
+++ src/components/step/Step.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+import styles from "./Step.module.scss";
+import classNames from "classnames";
 
 export default function Step({ number, title, description }) {
   const Item = styled(Paper)(({ theme }) => ({
@@ -15,8 +15,8 @@ export default function Step({ number, title, description }) {
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <Item>{number}</Item>
+      <Grid item xs={8} className={classNames(styles.numbers)}>
+        <Item className={classNames(styles.numbers)}>{number}</Item>
       </Grid>
       <Grid item xs={4}>
         <Item>{title}</Item>
