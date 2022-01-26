d0eb64ddc7c7525ef4275df48c3c5484f66602ed
Yordan <yordan_yordanov001@abv.bg>
2022-01-26 20:19:47 +0200

    img fix

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import Container from "@mui/material/Container";
-import classNames from "classnames";
-import styles from "./Featured.module.scss";
-import { useRouter } from "next/router";
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -15,81 +11,74 @@ function srcset(image, size, rows = 1, cols = 1) {
   };
 }
 
-export default function Featured({
-  items = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
-      title: "Breakfast",
-      rows: 2,
-      cols: 3,
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
-      title: "Burger",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
-      title: "Camera",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
-      title: "Coffee",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
-      title: "Hats",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
-      title: "Honey",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
-      title: "Basketball",
-      href: "/about",
-    },
-  ],
-}) {
-  const router = useRouter();
-
+export default function Featured() {
   return (
-    <div className={classNames(styles.wrapper)}>
-      <Container className={classNames(styles.container)} maxWidth="xl">
-        <ImageList
-          sx={{ width: 500, height: 450 }}
-          variant="quilted"
-          cols={4}
-          rowHeight={121}
+    <ImageList
+      sx={{ width: 500, height: 450 }}
+      variant="quilted"
+      cols={4}
+      rowHeight={121}
+    >
+      {itemData.map((item) => (
+        <ImageListItem
+          key={item.image}
+          cols={item.cols || 1}
+          rows={item.rows || 1}
         >
-          {items.map((item) => (
-            <ImageListItem
-              key={item.img}
-              cols={item.cols || 1}
-              rows={item.rows || 1}
-            >
-              <img
-                {...srcset(item.image, 121, item.rows, item.cols)}
-                alt={item.title}
-                loading="lazy"
-                onClick={() => router.push("/about")}
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </Container>
-    </div>
+          <img
+            {...srcset(item.image, 121, item.rows, item.cols)}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 }
+
+const itemData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
+    title: "Breakfast",
+    rows: 2,
+    cols: 3,
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
+    title: "Burger",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
+    title: "Camera",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
+    title: "Coffee",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
+    title: "Hats",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
+    title: "Honey",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
+    title: "Basketball",
+    href: "/about",
+  },
+];
