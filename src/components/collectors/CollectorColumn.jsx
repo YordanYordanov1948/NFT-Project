b536b7e436c76ce9ab4862566ab576d108fcc2a3
Yordan <yordan_yordanov001@abv.bg>
2022-01-31 12:41:04 +0200

    fix

--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import Collector from "./Collector";
-import styles from "./CollectorColumn.module";
-import classNames from "classnames";
 
 export default function CollectorColumn({ items = [] }) {
   return (
     <div>
       {items.map((item, i) => (
-        <Collector
-          key={i}
-          item={item}
-          index={i + 1}
-          type={i % 2 !== 0 ? classNames(styles.light) : ""}
-        ></Collector>
+        <Collector key={i} item={item} index={items}></Collector>
       ))}
     </div>
   );
