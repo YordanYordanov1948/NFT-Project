4beb54b2208e0e68b3053a78397e40734c26a520
Yordan <yordan_yordanov001@abv.bg>
2022-01-31 12:55:43 +0200

    fix

--- src/components/collectors/Collector.jsx
+++ src/components/collectors/Collector.jsx
@@ -1,8 +1,5 @@
-import classNames from "classnames";
 import React from "react";
 import User from "../user/User";
-import styles from "./Example.module.scss";
-import classNames from "classnames";
 
 export default function Collector({ name, avatar, verified, type, nftsCount }) {
   return (
@@ -11,7 +8,7 @@ export default function Collector({ name, avatar, verified, type, nftsCount }) {
         name={name}
         avatar={avatar}
         verified={verified}
-        type={type ? classNames(styles.light) : ""}
+        type={type}
         nftsCount={nftsCount}
       />
     </div>
