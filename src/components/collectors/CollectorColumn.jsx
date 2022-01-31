d447f38ed3af3b50abde2f89b6055e5885321722
Yordan <yordan_yordanov001@abv.bg>
2022-01-31 16:22:30 +0200

    fix

--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -4,14 +4,18 @@ import classNames from "classnames";
 import styles from "./CollectorColumn.module.scss";
 
 export default function CollectorColumn({ items = [] }) {
-  <div className={classNames(styles.container)}>
-    {items.map((item, i) => (
-      <Collector
-        key={i}
-        item={item}
-        index={i + 1}
-        type={i % 2 !== 0 ? classNames(styles.light) : classNames(styles.dark)}
-      ></Collector>
-    ))}
-  </div>;
+  return (
+    <div className={classNames(styles.container)}>
+      {items.map((item, i) => (
+        <Collector
+          key={i}
+          item={item}
+          index={i + 1}
+          type={
+            i % 2 !== 0 ? classNames(styles.light) : classNames(styles.dark)
+          }
+        ></Collector>
+      ))}
+    </div>
+  );
 }
