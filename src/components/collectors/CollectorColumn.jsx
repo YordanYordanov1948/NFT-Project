d89ddc0fef771f4a88b6fcbc5d4f0349f762a47a
Yordan <yordan_yordanov001@abv.bg>
2022-02-02 18:06:30 +0200

    fix

--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Collector from "./Collector";
-import classNames from "classnames";
 import styles from "./CollectorColumn.module.scss";
 
 export default function CollectorColumn({ items = [] }) {
