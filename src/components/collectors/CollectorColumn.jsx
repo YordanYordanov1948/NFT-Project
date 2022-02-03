38919801c911cda81dc1245cee8a6e4758ab3237
Yordan <yordan_yordanov001@abv.bg>
2022-02-03 19:15:13 +0200

    fix

--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -5,7 +5,7 @@ export default function CollectorColumn({ items = [] }) {
   return (
     <div className="container">
       {items.map((item, i) => (
-        <Collector key={i} item={item} index={i + 1} type={type}></Collector>
+        <Collector key={i} item={item} index={i + 1}></Collector>
       ))}
     </div>
   );
