0f165b0d811817c75bd06bf674365b838a140c9d
Yordan <yordan_yordanov001@abv.bg>
2022-02-07 18:49:42 +0200

    prop fix

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "./ProductImage.module.scss";
 import classNames from "classnames";
 
-export default function ProductImage({ url }) {
+export default function ProductImage({ URL }) {
   return (
     <div className={classNames(styles["product-image"])}>
-      <img className="image" src={url}></img>
+      <img className="image" src={URL}></img>
     </div>
   );
 }
