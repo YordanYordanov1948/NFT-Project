4d3d75c1043a41de13a2f66a604daaf2f9e08514
Yordan <yordan_yordanov001@abv.bg>
2022-02-07 18:28:03 +0200

    fix

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 export default function ProductImage({ url }) {
   return (
     <div className={classNames(styles[`product-image`])}>
-      <img className={classNames(styles[`image`])} />
+      <img src={url} className="image"></img>
     </div>
   );
 }
