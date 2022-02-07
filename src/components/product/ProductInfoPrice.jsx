07c4acf2b0369bfa438c0ab4b7877e32c7614c94
Yordan <yordan_yordanov001@abv.bg>
2022-02-07 20:24:51 +0200

    ProductInfoStatus component created

--- src/components/product/ProductInfoPrice.jsx
+++ src/components/product/ProductInfoPrice.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./ProductInfoPrice.module.scss";
 import classNames from "classnames";
 
-export default function ProductInfoPrice({ amount = 0 }) {
+export default function ProductInfoPrice({ amount = 0, currency }) {
   return (
-    <div className={classNames(styles["product-info-price"])}>{amount}</div>
+    <div className={classNames(styles["product-info-price"])}>
+      {amount} {currency}
+    </div>
   );
 }
