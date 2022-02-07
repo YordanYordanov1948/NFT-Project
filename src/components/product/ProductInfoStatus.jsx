299e52e0b8b539512e3e6ac51f10dec127998922
Yordan <yordan_yordanov001@abv.bg>
2022-02-07 20:37:50 +0200

    bug fix

--- src/components/product/ProductInfoStatus.jsx
+++ src/components/product/ProductInfoStatus.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./ProductInfoPrice.module.scss";
+import styles from "./ProductInfoStatus.module.scss";
 import classNames from "classnames";
 import Chip from "@mui/material/Chip";
 
