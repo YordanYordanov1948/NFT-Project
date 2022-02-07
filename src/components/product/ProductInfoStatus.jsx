4cf0498778232bebde2b0fcaa2b4de1ac6af65b3
Yordan <yordan_yordanov001@abv.bg>
2022-02-08 00:51:09 +0200

    fix

--- src/components/product/ProductInfoStatus.jsx
+++ src/components/product/ProductInfoStatus.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import classNames from "classnames";
 import styles from "./ProductInfoStatus.module.scss";
+import Chip from "@mui/material/Chip";
 
 export default function ProductInfoStatus() {
   return (
     <div className={classNames(styles["product-info-status"])}>
-      <div className={classNames(styles.status)}></div>
+      <Chip
+        className={classNames(styles.status)}
+        label="Chip Outlined"
+        variant="outlined"
+      />
     </div>
   );
 }
