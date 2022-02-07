8f34e3f671f909c8204442f438ff72bf1760efed
Yordan <yordan_yordanov001@abv.bg>
2022-02-07 13:49:01 +0200

    topcollectors component added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import Header from "../src/components/header/Header";
 import CollectorColumn from "../src/components/collectors/CollectorColumn";
 import Collector from "../src/components/collectors/Collector";
+import TopCollectors from "../src/components/collectors/TopCollectors";
 export default function Index() {
   return (
     <div>
       <Header />
       <CollectorColumn />
       <Collector />
+      <TopCollectors />
     </div>
   );
 }
