bebdfc4a57aa751a3f3714d1507c31da74090da2
Yordan <yordan_yordanov001@abv.bg>
2022-02-07 15:52:13 +0200

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,21 @@
-import About from "./about";
+import Header from "../src/components/header/Header";
+import Featured from "../src/components/featured/Featured";
+import Trending from "../src/components/trending/Trending";
+import TopCollectors from "../src/components/collectors/TopCollectors";
+import How from "../src/components/how/How";
+import Auctions from "../src/components/auctions/Auctions";
+import Footer from "../src/components/footer/Footer";
 
 export default function Index() {
   return (
     <div>
-      <About />
+      <Header />
+      <Featured />
+      <Trending />
+      <TopCollectors />
+      <How />
+      <Auctions />
+      <Footer />
     </div>
   );
 }
