5d2cb9c9f00ff3e3765136443424472af6f6b5e5
Yordan <yordan_yordanov001@abv.bg>
2022-02-08 08:59:54 +0200

    fix

--- src/components/product/ProductInfoCreator.jsx
+++ src/components/product/ProductInfoCreator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./ProductInfoCreator.module.scss";
 import classNames from "classnames";
 import User from "../user/User";
+import Typography from "@mui/material/Typography";
 
 export default function ProductInfoCreator({
   name = "",
@@ -9,9 +10,11 @@ export default function ProductInfoCreator({
   verified = false,
 }) {
   return (
-    <div className={classNames["product-info-creator"]}>
-      <h1 className={classNames.title}>Creator</h1>
-      <div className={classNames["user-container"]}>
+    <div className={classNames(styles["product-info-creator"])}>
+      <Typography variant="h1" className={classNames(styles.title)}>
+        Creator
+      </Typography>
+      <div className={classNames(styles["user-container"])}>
         <User name={name} verified={verified} avatar={avatar} />
       </div>
     </div>
