7b62131b79ebd6c377d073ce788f50c111b0bd09
Yordan <yordan_yordanov001@abv.bg>
2022-02-08 14:04:29 +0200

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState, Fragment, useEffect } from "react";
 import Header from "../src/components/header/Header";
 import Featured from "../src/components/featured/Featured";
 import Trending from "../src/components/trending/Trending";
@@ -5,42 +6,33 @@ import TopCollectors from "../src/components/collectors/TopCollectors";
 import How from "../src/components/how/How";
 import Auctions from "../src/components/auctions/Auctions";
 import Footer from "../src/components/footer/Footer";
-import { useState, useEffect } from "react";
 import dataFeatured from "../data/featured.json";
 import dataTrending from "../data/trending.json";
 import dataUsers from "../data/users.json";
 import dataNfts from "../data/nfts.json";
 
-export default function Index(props) {
+export default function Index() {
   const [featuredCards, setFeaturedCards] = useState([]);
-  useEffect(() => {
-    setFeaturedCards(dataFeatured);
-  }, []);
-
-  const [trending, setTrending] = useState([]);
-  useEffect(() => {
-    setTrending(dataTrending);
-  }, []);
-
+  const [trendingCards, setTrendingCards] = useState([]);
   const [users, setUsers] = useState([]);
+  const [nfts, setNfts] = useState([]);
   useEffect(() => {
+    setFeaturedCards(dataTrending);
+    setTrendingCards(dataFeatured);
     setUsers(dataUsers);
+    setNfts(dataNfts);
   }, []);
-
-  const [ntfs, setNtfs] = useState([]);
-  useEffect(() => {
-    setNtfs(dataNfts);
-  }, []);
-
   return (
-    <div>
+    <Fragment>
       <Header />
-      <Featured items={props.items} />
-      <Trending cards={props.cards} />
-      <TopCollectors collectors={props.collectors} />
-      <How />
-      <Auctions cards={props.cards} />
+      <div className="app">
+        <Featured items={featuredCards} />
+        <Trending cards={trendingCards} />
+        <TopCollectors collectors={users} />
+        <How />
+        <Auctions cards={nfts} />
+      </div>
       <Footer />
-    </div>
+    </Fragment>
   );
 }
