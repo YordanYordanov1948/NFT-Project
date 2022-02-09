641147e5f63cb63c94bb13f24a6f767ff0ca906f
Yordan <yordan_yordanov001@abv.bg>
2022-02-10 00:12:52 +0200

    fix

---
+++ pages/product/id/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../../../src/components/header/Header";
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
