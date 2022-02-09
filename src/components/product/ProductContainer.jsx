2f147015bd745d30ade608094d662a2be797800e
Yordan <yordan_yordanov001@abv.bg>
2022-02-09 12:21:27 +0200

    fix

--- src/components/product/ProductContainer.jsx
+++ src/components/product/ProductContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import styles from "./ProductTabs.module.scss";
+import styles from "./ProductContainer.module.scss";
 
 export default function ProductContainer() {
   return <div className={classNames(styles["product-container"])}></div>;
