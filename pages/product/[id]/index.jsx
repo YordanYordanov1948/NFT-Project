0c0db59ec0a9946eec9503f26357c8683f3e98c7
Yordan <yordan_yordanov001@abv.bg>
2022-02-10 09:33:49 +0200

    fix

--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../../src/components/header/Header";
 
-export default function index() {
+export default function Index() {
   return (
     <div>
       <Header />
