4e017ebe329c143d05ab6492cd35818d26301c17
Yordan <yordan_yordanov001@abv.bg>
2022-02-14 19:04:03 +0200

    fix

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Hero.module.scss";
+import classNames from "classnames";
+import { Typography } from "@mui/material";
+
+export default function Hero({ text }) {
+  return (
+    <div className={classNames(styles.hero)}>
+      <Typography variant="h1" className={classNames(styles.text)}>
+        {text}
+      </Typography>
+    </div>
+  );
+}
