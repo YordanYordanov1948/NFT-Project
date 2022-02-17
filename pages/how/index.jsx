f9746eb2840782d3cd723b0b24f5d557eb5cc322
Yordan <yordan_yordanov001@abv.bg>
2022-02-17 14:28:56 +0200

    fix

--- pages/how/index.jsx
+++ pages/how/index.jsx
@@ -1,14 +1,16 @@
-import Description from "../../src/components/description/Description";
-import Footer from "../../src/components/footer/Footer";
+import React from "react";
 import Header from "../../src/components/header/Header";
 import Hero from "../../src/components/hero/Hero";
-export default function index({ text, image }) {
+import Description from "../../src/components/description/Description";
+import Footer from "../../src/components/footer/Footer";
+
+export default function How(props) {
   return (
-    <>
+    <div>
       <Header />
-      <Hero text={text} />
-      <Description text={text} image={image} />
+      <Hero text={props.text} />
+      <Description text={props.text} image={props.image} />
       <Footer />
-    </>
+    </div>
   );
 }
