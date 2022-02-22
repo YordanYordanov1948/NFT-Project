3c7c1bf11da1bedd8f6b63b9fb9da3a668c8b793
Yordan <yordan_yordanov001@abv.bg>
2022-02-22 10:47:54 +0200

    fix

--- src/components/profile/ProfileCollection.jsx
+++ src/components/profile/ProfileCollection.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import classNames from "classnames";
 import styles from "./ProfileCollection.module.scss";
-import Grid from "@mui/material/Grid";
-import { Typography } from "@mui/material";
+import classNames from "classnames";
+import { Container, Grid, Typography } from "@mui/material";
 import ProfileCollectionFilters from "./ProfileCollectionFilters";
 import Card from "../card/Card";
-import User from "../user/User";
 
 export default function ProfileCollection({
-  user = { name: "J", info: "someInfo" },
-  filters = "name",
+  user,
+  filters,
   items = [
     {
       name: "String",
@@ -20,64 +18,36 @@ export default function ProfileCollection({
       price: "~12.2",
       currency: "BTC",
     },
-    {
-      name: "String",
-      likes: 0,
-      user: "Ronny",
-      mediaUrl: "images/nft.jpg",
-      timeLeft: 100000000,
-      price: "~12.2",
-      currency: "ETH",
-    },
-    {
-      name: "String",
-      likes: 0,
-      user: "Tom",
-      mediaUrl: "images/nft.jpg",
-      timeLeft: 100000000,
-      price: "~12.2",
-      currency: "LTC",
-    },
-    {
-      name: "String",
-      likes: 0,
-      user: "Mike",
-      mediaUrl: "images/nft.jpg",
-      timeLeft: 100000000,
-      price: "~12.2",
-      currency: "BNB",
-    },
   ],
 }) {
   return (
     <div className={classNames(styles["profile-collection"])}>
-      <User name={user.name} />
-      <Grid container className={classNames(styles.container)}>
-        <Grid item xs={3}>
-          <Typography variant="h3">Collection</Typography>
+      <Container>
+        <Grid container>
+          <Grid item xs={3}>
+            <Typography variant="h3">Collections</Typography>
+          </Grid>
+          <Grid item xs={9}>
+            <ProfileCollectionFilters filters={filters} />
+          </Grid>
         </Grid>
-        <Grid item xs={9}>
-          <ProfileCollectionFilters filters={filters} />
-        </Grid>
-      </Grid>
-      <Grid container spacing={1}>
-        {items.map((item, index) => {
-          return (
+        <Grid container>
+          {items.map((card, index) => (
             <Grid item xs={3}>
               <Card
                 key={index}
-                name={item.name}
-                likes={item.likes}
-                user={item.user}
-                mediaUrl={item.mediaUrl}
-                timeLeft={item.timeLeft}
-                price={item.price}
-                currency={item.currency}
+                name={card.name}
+                likes={card.likes}
+                mediaUrl={card.source?.url}
+                user={user}
+                price={card.price}
+                currency={card.currency}
+                timeLeft={card.auction_end}
               />
             </Grid>
-          );
-        })}
-      </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
