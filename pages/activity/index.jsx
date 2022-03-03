d5f9388fca5f7d3057220e5dc4a5f05c8c02b5cc
Yordan <yordan_yordanov001@abv.bg>
2022-03-03 19:53:52 +0200

    fix

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -5,18 +5,18 @@ import ActivityFilter from "../../src/components/activity/ActivityFilters";
 import ActivityList from "../../src/components/activity/ActivityList";
 import Footer from "../../src/components/footer/Footer";
 
-let url = process.env.apiUrl;
-
-const [activity, activityFilters] = useState();
-
-useEffect(async () => {
-  const result = await fetch("https://nft-auction.herokuapp.com/activities")
-    .then((response) => response.json())
-    .then((res) => res);
-  activityFilters(result);
-}, []);
-
 export default function Activity() {
+  let url = process.env.apiUrl;
+
+  const [activity, activityFilters] = useState();
+
+  useEffect(async () => {
+    const result = await fetch("https://nft-auction.herokuapp.com/activities")
+      .then((response) => response.json())
+      .then((res) => res);
+    activityFilters(result);
+  }, []);
+
   return (
     <div>
       <Header />
