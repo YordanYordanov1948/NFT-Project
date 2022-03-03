20d290f4b7cf0598a8a4f3596ff68f5ce4979146
Yordan <yordan_yordanov001@abv.bg>
2022-03-03 19:20:55 +0200

    explore API added

--- pages/explore/index.jsx
+++ pages/explore/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../src/components/header/Header";
 import ExploreTitle from "../../src/components/explore/ExploreTitle";
 import ExploreFilters from "../../src/components/explore/ExploreFilters";
@@ -8,6 +8,25 @@ import Card from "../../src/components/card/Card";
 import nfts from "../../data/nfts.json";
 
 export default function Explore() {
+  let url = process.env.apiUrl;
+
+  const [nfts, setNfts] = useState();
+  const [nftFilters, setNftFilters] = useState();
+
+  useEffect(async () => {
+    const result = await fetch("https://nft-auction.herokuapp.com/explore")
+      .then((response) => response.json())
+      .then((res) => res.nfts);
+    setNfts(result);
+  }, []);
+
+  useEffect(async () => {
+    const result = await fetch("https://nft-auction.herokuapp.com/explore")
+      .then((response) => response.json())
+      .then((res) => res.filters);
+    setNftFilters(result);
+  }, []);
+
   return (
     <div>
       <Container maxWidth="xl">
@@ -15,7 +34,7 @@ export default function Explore() {
         <Grid container>
           <Grid item>
             <ExploreTitle />
-            <ExploreFilters />
+            <ExploreFilters filters={nftFilters} nfts={nfts} />
           </Grid>
           <Grid item>
             <Card />
