bc6549ce2b163432dea2ac7c3fe3273f9da82561
Yordan <yordan_yordanov001@abv.bg>
2022-03-03 19:43:11 +0200

    Activity page API

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../src/components/header/Header";
 import Hero from "../../src/components/hero/Hero";
 import ActivityFilter from "../../src/components/activity/ActivityFilters";
 import ActivityList from "../../src/components/activity/ActivityList";
 import Footer from "../../src/components/footer/Footer";
+
+let url = process.env.apiUrl;
+
+const [activity, activityFilters] = useState();
+
+useEffect(async () => {
+  const result = await fetch("https://nft-auction.herokuapp.com/activities")
+    .then((response) => response.json())
+    .then((res) => res);
+  activityFilters(result);
+}, []);
+
 export default function Activity() {
   return (
     <div>
       <Header />
       <Hero text={"Activity"} />
-      <ActivityFilter />
+      <ActivityFilter activity={activity} />
       <ActivityList />
       <Footer />
     </div>
