8d0c0d78de145fee9e13bb2cd5918fc769d1548a
Yordan <yordan_yordanov001@abv.bg>
2022-03-07 19:46:14 +0200

    how page fix

--- pages/how/index.jsx
+++ pages/how/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Header from "../../src/components/header/Header";
+import { Container } from "@mui/material";
 import Hero from "../../src/components/hero/Hero";
 import Description from "../../src/components/description/Description";
 import Footer from "../../src/components/footer/Footer";
 
-export default function How(props) {
+export default function How() {
   return (
     <div>
       <Header />
-      <Hero text={props.text} />
-      <Description text={props.text} image={props.image} />
+      <Hero text="How it works" />
+      <Description
+        text="Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+    dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+    Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+    sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue."
+      />
       <Footer />
     </div>
   );
