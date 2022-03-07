fde9e045597a7a279425e2b4be5bce26e604f848
Yordan <yordan_yordanov001@abv.bg>
2022-03-07 15:55:43 +0200

    styling

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Header from "../../src/components/header/Header";
 import Hero from "../../src/components/hero/Hero";
-import ActivityFilter from "../../src/components/activity/ActivityFilters";
+import ActivityFilters from "../../src/components/activity/ActivityFilters";
 import ActivityList from "../../src/components/activity/ActivityList";
 import Footer from "../../src/components/footer/Footer";
 
 export default function Activity() {
   let url = process.env.apiUrl;
 
-  const [activity, activityFilters] = useState();
+  const [activity, setActivity] = useState([]);
+  const [activityFilters, setActivityFilters] = useState([]);
 
   useEffect(async () => {
     const result = await fetch("https://nft-auction.herokuapp.com/activities")
       .then((response) => response.json())
-      .then((res) => res);
-    activityFilters(result);
+      .then((res) => res.activities);
+    setActivity(result);
+  }, []);
+
+  useEffect(async () => {
+    const result = await fetch("https://nft-auction.herokuapp.com/activities")
+      .then((response) => response.json())
+      .then((res) => res.filters.sort);
+    setActivityFilters(result);
   }, []);
 
   return (
     <div>
       <Header />
       <Hero text={"Activity"} />
-      <ActivityFilter activity={activity} />
-      <ActivityList />
+      <ActivityFilters />
+      <ActivityList list={activity}></ActivityList>
       <Footer />
     </div>
   );
