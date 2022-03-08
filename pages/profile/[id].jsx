8a79cda4fc4e469fbaef21027f140b2290128fc6
Yordan <yordan_yordanov001@abv.bg>
2022-03-08 15:03:00 +0200

    fix

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,38 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "../../src/components/header/Header";
 import ProfileHero from "../../src/components/profile/ProfileHero";
 import ProfileUser from "../../src/components/profile/ProfileUser";
 import ProfileCollection from "../../src/components/profile/ProfileCollection";
 import Footer from "../../src/components/footer/Footer";
-import profile from "../../data/profile.json";
-import classNames from "classnames";
 
 export default function Profile() {
-  let url = process.env.apiUrl;
-
-  const [profile, setProfile] = useState();
-  const [profileFilters, setProfileFilters] = useState();
-
-  useEffect(async () => {
-    const result = await fetch("https://nft-auction.herokuapp.com/users/367")
-      .then((response) => response.json())
-      .then((res) => res);
-    setProfile(result);
-  }, []);
-
-  useEffect(async () => {
-    const result = await fetch("https://nft-auction.herokuapp.com/users/367")
-      .then((response) => response.json())
-      .then((res) => res.filters);
-    setProfileFilters(result);
-  }, []);
-
   return (
     <div>
       <Header />
       <ProfileHero />
-      <ProfileUser name="NFT" info="NFT" />
-      <ProfileCollection user={profile} filters={profileFilters} />
+      <ProfileUser />
+      <ProfileCollection />
       <Footer />
     </div>
   );
