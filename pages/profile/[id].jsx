ddc7f88e96d3eb4d47e62d481ffc0c50b982ba28
Yordan <yordan_yordanov001@abv.bg>
2022-03-08 15:07:25 +0200

    API FIX

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../src/components/header/Header";
 import ProfileHero from "../../src/components/profile/ProfileHero";
 import ProfileUser from "../../src/components/profile/ProfileUser";
@@ -6,12 +6,31 @@ import ProfileCollection from "../../src/components/profile/ProfileCollection";
 import Footer from "../../src/components/footer/Footer";
 
 export default function Profile() {
+  let url = process.env.apiUrl;
+
+  const [profile, setProfile] = useState();
+  const [profileFilters, setProfileFilters] = useState();
+
+  useEffect(async () => {
+    const result = await fetch("https://nft-auction.herokuapp.com/users/367")
+      .then((response) => response.json())
+      .then((res) => res);
+    setProfile(result);
+  }, []);
+
+  useEffect(async () => {
+    const result = await fetch("https://nft-auction.herokuapp.com/users/367")
+      .then((response) => response.json())
+      .then((res) => res.filters);
+    setProfileFilters(result);
+  }, []);
+
   return (
     <div>
       <Header />
       <ProfileHero />
       <ProfileUser />
-      <ProfileCollection />
+      <ProfileCollection user={profile} filters={profileFilters} />
       <Footer />
     </div>
   );
--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./ProfileCollectionFilters.module.scss";
 import classNames from "classnames";
+import styles from "./ProfileCollectionFilters.module.scss";
 import {
   FormControl,
   Select,
@@ -10,58 +10,29 @@ import {
   TextField,
   InputAdornment,
 } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { useState } from "react";
+import Search from "@mui/icons-material/Search";
 
 export default function ProfileCollectionFilters({ filters }) {
-  const [sort, setSort] = useState(1);
-  const [price, setPrice] = useState(3);
-  const handleSort = (e) => {
-    setSort(e.target.value);
-  };
-  const handlePrice = (e) => {
-    setPrice(e.target.value);
-  };
-
   return (
     <div className={classNames(styles["profile-collection-filters"])}>
-      {filters && (
-        <Stack direction="row" spacing={2}>
-          <FormControl fullWidth>
-            <InputLabel>Sort by</InputLabel>
-            <Select label="Sort by" value={sort} onChange={handleSort}>
-              {filters.sort.map((s) => (
-                <MenuItem key={s.value} value={s.value}>
-                  {s.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel>Price range</InputLabel>
-            <Select label="Price range" value={price} onChange={handlePrice}>
-              {filters.price.map((p) => (
-                <MenuItem key={p.value} value={p.value}>
-                  {p.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField
-              label="Search"
-              variant="standard"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon style={{ color: "white" }} />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-        </Stack>
-      )}
+      <FormControl className={classNames(styles["form-control"])}>
+        <Select autoWidth="true" label="Sort by">
+          <InputLabel>{filters}</InputLabel>
+        </Select>
+      </FormControl>
+      <FormControl className={classNames(styles["price-range"])}>
+        <Select label="Price range"></Select>
+      </FormControl>
+      <TextField
+        className={classNames(styles["search-bar"])}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search></Search>
+            </InputAdornment>
+          ),
+        }}
+      ></TextField>
     </div>
   );
 }
